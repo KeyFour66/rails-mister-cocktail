22ee80a53b13fe3d019d566c4c3544fcf34ead72
Laurent Fourquet <fourquet@live.fr>
2020-03-05 17:31:30 +0100

    doseokmissdelete

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,41 @@
+class DosesController < ApplicationController
+
+before_action :set_dose, only: [:show, :edit, :update, :destroy]
+
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail_id = @cocktail.id
+    # @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+end
+
+
